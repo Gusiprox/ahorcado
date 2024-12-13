c2e7ce07ee30e615d4e6ab0d5e3cc90da8d1edb2
Gusiprox <gusiprox_079_patata@hotmail.com>
2024-12-13 19:30:51 +0100

    primero

---
+++ juego/main.java
@@ -0,0 +1,15 @@
+package juego;
+
+import java.awt.JobAttributes;
+
+import javax.swing.JOptionPane;
+
+public class main {
+
+    public static void main(String[] args) {
+        
+        JOptionPane.showMessageDialog(null,"|||||||||| \niiiiiiiiii \npppppppppp ");
+
+        
+    }
+}
